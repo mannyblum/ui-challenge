bbee55d4794a661dee971db995eee406b5554d0f
mannyblum <manny.blum@gmail.com>
2017-09-25 01:48:06 -0400

    rewrote a lot of the app to more efficient and adhere to better standards

--- src/App.js
+++ src/App.js
@@ -1,82 +1,77 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const PATH_BASE = 'https://api.themoviedb.org/3';
-const PATH_SEARCH = '/movie/popular';
-const API_KEY = '676b37a6ad0aaaa61a566c3097c60afe'; // this would be hidden in a normal application
+const TMDB = {
+  'pathBase': 'https://api.themoviedb.org/3',
+  'endpoint': '/movie/popular',
+  'apiKey': '676b37a6ad0aaaa61a566c3097c60afe'
+};
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      results: null,
+      results: [],
       page: 1,
-      isLoading: false,
+      initLoad: false,
       isLoadingMore: false
     };
 
-    this.fetchPopularMovies = this.fetchPopularMovies.bind(this);
+    this.fetchMovies = this.fetchMovies.bind(this);
 
     this.loadMore = this.loadMore.bind(this);
     this.onScroll = this.onScroll.bind(this);
   }
 
-  fetchPopularMovies(page) {
-    this.setState({ isLoading: true });
+  fetchMovies(pageToLoad) {
+    if (!pageToLoad) {
+      this.setState({ initLoad: true });
+    }
 
-    // Initial Load of data
-    fetch(`${PATH_BASE}${PATH_SEARCH}?api_key=${API_KEY}&page=${page}`)
+    let page = pageToLoad || this.state.page;
+    const path = `${TMDB.pathBase}${TMDB.endpoint}?api_key=${TMDB.apiKey}&page=${page}`;
+
+    fetch(path)
       .then(response => response.json())
       .then(result => {
-        this.setState({ results: result.results, isLoading: false });
-      })
-      .then(() => {
-        // sorta hacky, loads 2 pages worth of data because I can't scroll wit h just 1 set of results and API doesn't allow to set a limit of results
-        fetch(`${PATH_BASE}${PATH_SEARCH}?api_key=${API_KEY}&page=2`)
-        .then(response => response.json())
-        .then(result => {
-          this.setState({ results: this.state.results.concat(result.results), isLoadingMore: false })
-        })
-      })
-  ;
+        let newState = this.state.results.concat(result.results);
+        this.setState({ results: newState, initLoad: false, isLoadingMore: false, page: page });
+
+        if (document.body.offsetHeight < window.innerHeight) {
+          this.loadMore();
+        }
+      });
   }
 
   componentDidMount() {
-    this.fetchPopularMovies(1);
+    this.fetchMovies();
     window.addEventListener('scroll', this.onScroll, false);
   }
 
-  componentWillUnmount() {
-    window.removeEventListener('scroll', this.onScroll, false);
-  }
-
-  onScroll = () => {
-    if ( (window.innerHeight + window.scrollY) >= (document.body.offsetHeight - 500) && this.state.results.length && !this.state.isLoadingMore ) {
+  onScroll()  {
+    if ((window.innerHeight + window.scrollY) >= (document.body.offsetHeight - 500) && !this.state.isLoadingMore ) {
       this.loadMore();
     }
   }
 
   loadMore() {
-    this.setState({page: this.state.page + 1, isLoadingMore: true});
+    let page = this.state.page + 1;
 
-    // TODO: DRY THIS UP - reuse `fetchPopularMovies`
-    fetch(`${PATH_BASE}${PATH_SEARCH}?api_key=${API_KEY}&page=${this.state.page + 1}`)
-      .then(response => response.json())
-      .then(result => {
-        this.setState({ results: this.state.results.concat(result.results), isLoadingMore: false })
-      });
+    this.setState({page: page, isLoadingMore: true});
+
+    this.fetchMovies(page);
   }
 
   render() {
-    const { results, isLoading } = this.state;
+    const { results, initLoad} = this.state;
 
     return (
       <div className="flix">
         <div className="flix-header">
           <h1>Popular</h1>
         </div>
-        { (isLoading || !results) ?
+        {(initLoad|| !results) ?
           <Loading /> :
           <Movies list={results} />
         }
@@ -85,27 +80,33 @@ class App extends Component {
   }
 }
 
-const Loading = () =>
-  <div className="movies">
-    <h1>Loading ...</h1>
-  </div>
+function Loading() {
+  return (
+    <div className="movies">
+      <h1>Loading ...</h1>
+    </div>
+  );
+}
 
-const Movies = ({list}) =>
-  <div className="movies">
-    { list.map(function(item) {
-      if (!item.backdrop_path) {
-        return false;
-      }
+function Movies({list}) {
+  return (
+    <div className="movies">
+      { list.map(function(item) {
+        if (!item.backdrop_path) {
+          return false;
+        }
 
-      return (
-        <div key={item.id} className="movie-holder">
-          <div className="movie">
-            <span>{item.title}</span>
-            <img src={'https://image.tmdb.org/t/p/w1280/' + item.backdrop_path} alt={item.title} />
+        return (
+          <div key={item.id} className="movie-holder">
+            <div className="movie">
+              <span>{item.title}</span>
+              <img src={'https://image.tmdb.org/t/p/w1280/' + item.backdrop_path} alt={item.title} />
+            </div>
           </div>
-        </div>
-      )
-    })}
-  </div>
+        );
+      })}
+    </div>
+  );
+}
 
 export default App;
